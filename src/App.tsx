c416f6241115496b738edc0d6e10f81853a58984
egoing <egoing@gmail.com>
2023-06-08 10:26:20 +0900

    컴포넌트 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.css";
 
+function Counter() {
+  return (
+    <div>
+      <h1>Counter</h1>
+      <button>+</button> 👉 0
+    </div>
+  );
+}
+
 function App() {
-  return <div>Hello React</div>;
+  return (
+    <div>
+      <Counter></Counter>
+    </div>
+  );
 }
 
 export default App;
